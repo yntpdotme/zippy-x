7bdf334ce3b42ff249d815b14f5cb5e16f48a0ee
Akash Kadlag <akashkadlag14@gmail.com>
2024-04-03 17:16:48 +0530

    feat(backend): add global middlewares for enhanced backend security

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,50 @@
 import express from 'express';
+import cors from 'cors';
+import requestIp from 'request-ip';
+import helmet from 'helmet';
+import compression from 'compression';
+import hpp from 'hpp';
+import rateLimit from 'express-rate-limit';
 import cookieParser from 'cookie-parser';
 
 import {morganMiddleware} from './config/index.js';
+import {ApiError} from './api/common/utils/ApiError.js';
 
 const app = express();
 
+const corsOptions = {
+  origin:
+    process.env.CORS_ORIGIN === '*' ? '*' : process.env.CORS_ORIGIN?.split(','),
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 100, // Limit each IP to 100 requests per minute
+  standardHeaders: true,
+  legacyHeaders: false,
+  keyGenerator: (req, res) => {
+    return req.clientIp; // IP address from requestIp.mw(), as opposed to req.ip
+  },
+  handler: (_, __, ___, options) => {
+    throw new ApiError(
+      options.statusCode || 500,
+      `There are too many requests. You are only allowed ${
+        options.max
+      } requests per ${options.windowMs / 60000} minutes`
+    );
+  },
+});
+
 // global middlewares
+app.use(cors(corsOptions));
+app.use(helmet());
+app.use(compression());
+app.use(hpp());
+app.use(requestIp.mw());
+app.use('/api/', limiter);
 app.use(express.json({limit: '16kb'}));
 app.use(cookieParser());
 app.use(morganMiddleware);
@@ -14,7 +53,7 @@ app.use(morganMiddleware);
 import {healthCheckRouter} from './api/common/routes/index.js';
 import {errorHandler} from './api/common/middlewares/index.js';
 
-// healthcheck   
+// healthcheck
 app.use('/api/healthcheck', healthCheckRouter);
 
 // error handling middleware
