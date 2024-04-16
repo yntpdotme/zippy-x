736cae921d9b0073529218a7209f4228eab015f8
Akash Kadlag <akashkadlag14@gmail.com>
2024-04-16 19:49:28 +0530

    feat(frontend): implement guest signin functionality

--- client/src/features/authentication/components/GuestSignin.jsx
+++ client/src/features/authentication/components/GuestSignin.jsx
@@ -1,12 +1,30 @@
 import {useNavigate} from 'react-router-dom';
 
+import {AuthService} from '@features/authentication';
 import {Button} from '@components/ui';
 
 const GuestSignin = ({label}) => {
   const navigate = useNavigate();
 
   const handleGuestSignIn = async () => {
-    navigate('/dashboard');
+    try {
+      const formData = {
+        email: 'guest@email.com',
+        password: 'Guest@1',
+      };
+
+      await AuthService.signin(formData);
+
+      navigate('/dashboard');
+    } catch (error) {
+      let errorMessage = 'An unexpected error occurred.';
+
+      if (error?.response?.data?.message) {
+        errorMessage = error.response.data.message;
+      }
+
+      console.log(errorMessage);
+    }
   };
 
   return (
